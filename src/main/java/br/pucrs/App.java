6b0a57c1197f5db6dd72a5cd7606f03affbffed7
Jeniffer Klein <56811604+JenifferKlein@users.noreply.github.com>
2023-03-14 21:42:17 +0000

    exerc 2

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -6,29 +6,28 @@ import java.util.Random;
  */
 public class App 
 {
-    long maxVal2(long A[], int init, int end) {  
+    static long maxVal2(long A[], int init, int end) {
         if (end - init <= 1)
-            return max(A[init], A[end]);  
+            return Math.max(A[init], A[end]);
         else {
-              int m = (init + end)/2;
-              long v1 = maxVal2(A,init,m);   
-              long v2 = maxVal2(A,m+1,end);  
-              return max(v1,v2);
-             }
+            int m = (init + end) / 2;
+            long v1 = maxVal2(A, init, m);
+            long v2 = maxVal2(A, m + 1, end);
+            return Math.max(v1, v2);
+        }
     }
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         Random random = new Random();
         int init = 0;
-        int end = 32; 
-        long A[] = new long [32];
-        System.out.println(maxVal2);
+        int end = 10;
+        long A[] = new long[11];
 
-
-        for(int i = 0; i < A.length; i++){
-            A[i] = random.nextLong(1000);
+        for (int i = 0; i < A.length; i++) {
+            A[i] = random.nextInt(1000);
             System.out.println(A[i]);
         }
+
+        System.out.println("teste "+maxVal2(A, end, init));
     }
 }
