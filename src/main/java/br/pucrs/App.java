c18b22c18e3e6b055dc67f56c91160a83c53e5a7
lucasyk8 <80711078+lucasyk8@users.noreply.github.com>
2023-03-16 21:45:16 +0000

    exercicio 3 ta contando gustavo

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -6,6 +6,14 @@ import java.util.Random;
  */
 public class App 
 {
+    static long maxVal1(long A[], int n) {  
+        long max = A[0];
+        for (int i = 1; i < n; i++) {  
+            if( A[i] > max ) 
+               max = A[i];
+        }
+        return max;
+    }
     static long maxVal2(long A[], int init, int end) {
         if (end - init <= 1)
             return Math.max(A[init], A[end]);
@@ -14,20 +22,23 @@ public class App
             long v1 = maxVal2(A, init, m);
             long v2 = maxVal2(A, m + 1, end);
             return Math.max(v1, v2);
-        }
+        }  
     }
 
     public static void main(String[] args) {
         Random random = new Random();
         int init = 0;
         int end = 10;
-        long A[] = new long[11];
+        long A[] = new long[2048];
 
         for (int i = 0; i < A.length; i++) {
-            A[i] = random.nextInt(1000);
+            A[i] = random.nextInt(10000);
             System.out.println(A[i]);
         }
-
-        System.out.println("teste "+maxVal2(A, end, init));
+        long start = System.currentTimeMillis();
+        System.out.println("teste "+maxVal2(A, init, end));
+        long tempoFinal = System.currentTimeMillis();
+        System.out.println((tempoFinal - start) / 1000d);
+        
     }
 }
