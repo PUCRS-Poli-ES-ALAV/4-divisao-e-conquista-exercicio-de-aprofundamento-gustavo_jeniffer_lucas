da0a8f9b940cffda6417c919c40e0d27e8666cd3
Gustavo Beche Lopes <gustavo.beche@gmail.com>
2023-03-21 21:01:23 +0000

    insere 4 exec

--- main.py
+++ main.py
@@ -14,3 +14,25 @@ def mergeSort(arr):
     return result + min + max
 
 print(mergeSort([2,6,3,12,65,23,54,12,321,2]))
+
+def MULTIPLY(x, y, n): 
+    if (n == 1):
+        return x * y
+
+    m = n // 2
+    a = x // (2 ** m)
+    b = x % (2 ** m)
+    c = y // (2 ** m)
+    d = y % (2 ** m)
+
+    e = MULTIPLY(a, c, m)
+    f = MULTIPLY(b, d, m)
+    g = MULTIPLY(b, c, m)
+    h = MULTIPLY(a, d, m)
+
+    return (2 ** (2 * m) * e) + (2 ** m * (g + h)) + f
+
+# teste-o para os 3 casos de valores inteiros: com 4 bits, com 16 bits e com 64 bits. Nestes testes, contabilize o número de iterações que o algoritmo executa, e o tempo gasto.
+print(MULTIPLY(15, 15, 4))
+print(MULTIPLY(65535, 65535, 16))
+print(MULTIPLY(18446744073709551615, 18446744073709551615, 64))
