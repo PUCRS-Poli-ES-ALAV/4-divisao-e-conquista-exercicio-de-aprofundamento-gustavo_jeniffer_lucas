63f039bf8fc89e8fa530bd8049b50709d4ce5edb
Jeniffer Klein <56811604+JenifferKlein@users.noreply.github.com>
2023-03-21 21:21:49 +0000

    ta interagindo gustavo

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -6,6 +6,7 @@ import java.util.Random;
  */
 public class App 
 {
+    static int cont = 0;
     static long maxVal1(long A[], int n) {  
         long max = A[0];
         for (int i = 1; i < n; i++) {  
@@ -15,30 +16,34 @@ public class App
         return max;
     }
     static long maxVal2(long A[], int init, int end) {
+        cont ++;
         if (end - init <= 1)
             return Math.max(A[init], A[end]);
         else {
-            int m = (init + end) / 2;
+                        int m = (init + end) / 2;
             long v1 = maxVal2(A, init, m);
             long v2 = maxVal2(A, m + 1, end);
             return Math.max(v1, v2);
         }  
+        
     }
 
     public static void main(String[] args) {
+        //32, 2048 e 1.048.576
         Random random = new Random();
         int init = 0;
-        int end = 10;
-        long A[] = new long[2048];
+        long A[] = new long[32];
+        int end = A.length - 1;
 
         for (int i = 0; i < A.length; i++) {
             A[i] = random.nextInt(10000);
             System.out.println(A[i]);
         }
         long start = System.currentTimeMillis();
-        System.out.println("teste "+maxVal2(A, init, end));
+        System.out.println("maior numero: " + maxVal2(A, init, end));
         long tempoFinal = System.currentTimeMillis();
-        System.out.println((tempoFinal - start) / 1000d);
+        System.out.println("tempo de execucao: " + (tempoFinal - start) / 1000d);
+        System.out.println("numero de passos: " + cont);
         
     }
 }
